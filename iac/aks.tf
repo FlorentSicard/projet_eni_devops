559b8d752ef6482f9966adf0d09f73153a243b47
Florent Sicard <sicardflorent@hotmail.fr>
2025-08-25 15:35:56 +0200

    Update CI

--- iac/aks.tf
+++ iac/aks.tf
@@ -11,7 +11,6 @@ resource "azurerm_kubernetes_cluster" "main" {
     name                = "default"
     node_count          = var.node_count
     vm_size             = var.node_vm_size
-    enable_auto_scaling = true
     min_count           = 1
     max_count           = 5
     os_disk_size_gb     = 30
